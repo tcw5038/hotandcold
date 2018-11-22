cd1f6ef19d35b3ea3d867ddc79b8b8723ca51684
Tyler Warrick <tyler@tylers-mbp.attlocal.net>
2018-11-21 17:25:41 -0800

    Added working tests

--- src/components/guess-button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function GuessButton(props) {
-    return (
-    <button type='submit' className='guessButton'>Guess!</button>
-  );
-}
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -1,65 +1,26 @@
 import React from 'react';
-import GuessButton from './guess-button';
-import ShowHint from './show-hint';
-import CurrentGuess from './current-guess';
-
 
 export default class GuessForm extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            num : Math.floor((Math.random() * 100) + 1),//returns a random number between 1 and 100
-            resultHint:'',
-            currentGuess: 1
-            //enter our state driven elements here
-        }//end of initial state
-    }//end of constructor
     
-    setNewGuess(event){
+    submitGuess(event) {
         event.preventDefault();
-        const guess = this.textInput.value;
-        console.log(guess);
-        this.setState({
-            currentGuess:guess
-        });
-        this.CheckGuess(guess);
-    }
-
-    CheckGuess(guess){
-        console.log(guess);
-        let difference = Math.abs(guess-this.state.num); //absolute value guarantees a positive number in this case
-        let resultHint;
-        if(difference === 0){
-            resultHint = 'You guessed right!';
-        }
-        else if (difference <= 5 && difference > 0){
-            resultHint = 'Getting pretty hot!';
-        }
-        else if (difference > 5 && difference <= 10){
-            resultHint = 'Warm, but not quite there';
+        if(this.props.setNewGuess && this.textInput.value >=1 && this.textInput.value <=100){
+            const value = this.textInput.value;
+            this.props.setNewGuess(value);
+            this.textInput.value = '';
         }
         else{
-            resultHint = 'Still cold';
+            this.textInput.value = '';
         }
-        console.log(resultHint);
-        this.setState({
-            resultHint,
-            currentGuess:guess
-        });
-    }//end of CheckGuess
+        
+	}
 
     render(){
         return(
-            <form onSubmit={event => this.setNewGuess(event)}>
+            <form onSubmit={event => this.submitGuess(event)}>
                 <input type="number" min={1} max={100} ref={input => this.textInput = input}/>
-                <GuessButton/>
-                <ShowHint value={this.state.resultHint} />
-                <CurrentGuess value={this.state.currentGuess} />
+                <button type='submit' className='guessButton'>Guess!</button>
             </form>
         );
     }
-    
-
-   //
-
-}//end of component
+}
